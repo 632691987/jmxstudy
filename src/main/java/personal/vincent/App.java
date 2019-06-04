fb054817a19613326fdbbdd6e2c4b95e1b276370
Guangwei Zhang <vincentzhang@outlook.es>
2019-06-04 10:11:30 +0200

    first complete

---
+++ src/main/java/personal/vincent/App.java
@@ -0,0 +1,13 @@
+package personal.vincent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
